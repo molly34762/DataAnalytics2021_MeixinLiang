ffed154934928fedf4f0d0c9411138c68ef69e42
molly34762 <meixinliang8@gmail.com>
2021-03-27 23:18:42 -0400

    Added group3

---
+++ Group3/lab1_rpart1.R
@@ -0,0 +1,4 @@
+require(rpart)
+Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
